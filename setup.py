65fd9147087a63ab374aa376a664cfb234a86ece
AlisaWF <766768148@qq.com>
2023-05-24 19:46:36 +0800

    update src

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 
 from setuptools import setup, find_packages  # noqa: H301
 
-NAME = "identity_service"
+NAME = "domain-paas-sdk-python"
 VERSION = "1.0.0"
 # To install the library, run the following
 #
