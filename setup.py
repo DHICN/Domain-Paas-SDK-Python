872892e19be3c856d3af0423d7f278ba4745d2e0
AlisaWF <766768148@qq.com>
2023-05-25 14:08:22 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "domain-paas-sdk-python"
-VERSION = "1.0.2"
+VERSION = "1.0.3"
 # To install the library, run the following
 #
 # python setup.py install
@@ -27,7 +27,7 @@ setup(
     url="",
     keywords=["sdk", "python", "domain-paas"],
     install_requires=REQUIRES,
-    packages=find_packages(exclude=["test", "tests"]),
+    packages=find_packages(where='src',exclude=["test", "tests"]),
     include_package_data=True,
     long_description="""\
     This template should help get you started developing with python
