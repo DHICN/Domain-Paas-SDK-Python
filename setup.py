31e8f554629dc5c4371c8e55279dbd9bf2fee7fd
AlisaWF <766768148@qq.com>
2023-05-25 13:19:20 +0800

    add license

--- setup.py
+++ setup.py
@@ -32,5 +32,6 @@ setup(
     long_description="""\
     This template should help get you started developing with python
     """,
-    package_dir={'':'src'}
+    package_dir={'':'src'},
+    license='MIT'
 )
