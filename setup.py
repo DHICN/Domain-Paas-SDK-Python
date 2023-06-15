4c8a0a754a26da107e2bec5dce38d2641ec87783
AlisaWF <766768148@qq.com>
2023-06-15 15:39:38 +0800

    dhicn_domain_paas_sdk_python 1.0.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "dhicn_domain_paas_sdk_python"
-VERSION = "1.0.8"
+VERSION = "1.0.1"
 # To install the library, run the following
 #
 # python setup.py install
@@ -17,20 +17,20 @@ VERSION = "1.0.8"
 # http://pypi.python.org/pypi/setuptools
 
 REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil"]
-with open('README.md', encoding='utf-8') as f:
-    readme = f.read()
+# with open('README.md', encoding='utf-8') as f:
+#     readme = f.read()
 
 setup(
     name=NAME,
     version=VERSION,
     author="dhi",
     author_email="wafe@dhigroup.com",
-    url="",
+    url="https://github.com/DHICN/Domain-Paas-SDK-Python",
     keywords=["sdk", "python", "domain-paas"],
     install_requires=REQUIRES,
     packages=find_packages(where='src',exclude=["test", "tests"]),
     include_package_data=True,
-    long_description=readme,
+    long_description="这是一个[DHI 中国 业务中台](https://online-products.dhichina.cn/) 的 Client SDK 开发辅助包，帮您快速通过我们的业务中台构建应用。",
     package_dir={'':'src'},
     license='MIT'
 )
