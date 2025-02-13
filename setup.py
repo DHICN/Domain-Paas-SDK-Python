dbd0549ba33b006f1352290d23b83be3c45c42b9
AlisaWF <766768148@QQ.COM>
2025-02-13 10:20:49 +0800

    Update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "dhicn_domain_paas_sdk_python"
-VERSION = "1.0.13"
+VERSION = "1.0.14"
 # To install the library, run the following
 #
 # python setup.py install
